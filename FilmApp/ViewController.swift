254582511b4c34b85dd4497092675e3a5e024dbd
Baris Celik <baris95celik@gmail.com>
2022-04-15 18:43:49 +0300

    intial commit

---
+++ FilmApp/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  FilmApp
+//
+//  Created by barış çelik on 15.04.2022.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
